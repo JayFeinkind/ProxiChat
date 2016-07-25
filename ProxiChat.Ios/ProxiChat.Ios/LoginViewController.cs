a27515e729a009f116f21e2961c5702f9385a351
Jason Feinkind <jfeinkind@hudsonalley.com>
2016-07-25 11:56:01 -0400

    added ui project

---
+++ ProxiChat.Ios/ProxiChat.Ios/LoginViewController.cs
@@ -0,0 +1,13 @@
+using Foundation;
+using System;
+using UIKit;
+
+namespace ProxiChat.Ios
+{
+    public partial class LoginViewController : UIViewController
+    {
+        public LoginViewController (IntPtr handle) : base (handle)
+        {
+        }
+    }
+}
---
+++ ProxiChat.Ios/ProxiChat.Ios/LoginViewController.designer.cs
@@ -0,0 +1,56 @@
+// WARNING
+//
+// This file has been generated automatically by Xamarin Studio from the outlets and
+// actions declared in your storyboard file.
+// Manual changes to this file will not be maintained.
+//
+using Foundation;
+using System;
+using System.CodeDom.Compiler;
+using UIKit;
+
+namespace ProxiChat.Ios
+{
+    [Register ("LoginViewController")]
+    partial class LoginViewController
+    {
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIImageView _backgroundImage { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIView _containerView { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UISwitch _passwordSwitch { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton _submitButton { get; set; }
+
+        void ReleaseDesignerOutlets ()
+        {
+            if (_backgroundImage != null) {
+                _backgroundImage.Dispose ();
+                _backgroundImage = null;
+            }
+
+            if (_containerView != null) {
+                _containerView.Dispose ();
+                _containerView = null;
+            }
+
+            if (_passwordSwitch != null) {
+                _passwordSwitch.Dispose ();
+                _passwordSwitch = null;
+            }
+
+            if (_submitButton != null) {
+                _submitButton.Dispose ();
+                _submitButton = null;
+            }
+        }
+    }
+}
