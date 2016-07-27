8f90ac6e92edb2edcefa7c4a1521383c9202d77e
Jason <JayFeinkind@gmail.com>
2016-07-26 22:08:44 -0400

    re-adding project / changing name of project

---
+++ ProxiChat.Mobile/ProxiChat.Mobile.Services/ApiService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProxiChat.Mobile.Services
+{
+    public class ApiService
+    {
+    }
+}
